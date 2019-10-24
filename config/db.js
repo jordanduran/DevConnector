f1fe768d4635adf2c60672525c9cbf489db5bb30
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2019-10-24 19:15:34 -0400

    Created route files

---
+++ config/db.js
@@ -0,0 +1,15 @@
+// const mongoose = require('mongoose');
+// const config = require('config');
+
+
+// const connectDB = aysnc () => {
+//     try {
+
+//     } catch(err) {
+//         console.error(err.message);
+//         // Exit process with failure
+//         process.exit(1)
+//     }
+// };
+
+// module.exports = connectDB;
